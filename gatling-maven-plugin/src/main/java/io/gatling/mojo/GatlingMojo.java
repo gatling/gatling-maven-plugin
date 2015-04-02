3cb576309e896675fa65c11fd945b06ec0518bb8
Pierre DAL-PRA <dalpra.pierre@gmail.com>
2015-04-02 22:39:36 +0200

    Update Scala version

--- gatling-maven-plugin/src/main/java/io/gatling/mojo/GatlingMojo.java
+++ gatling-maven-plugin/src/main/java/io/gatling/mojo/GatlingMojo.java
@@ -54,7 +54,7 @@ import static java.util.Collections.singletonList;
 public class GatlingMojo extends AbstractMojo {
 
   // Compiler constants
-  public static final String SCALA_VERSION = "2.11.5";
+  public static final String SCALA_VERSION = "2.11.6";
   public static final String COMPILER_MAIN_CLASS = "io.gatling.compiler.ZincCompiler";
   public static final List<String> ZINC_JVM_ARGS = singletonList("-Xss10M");
 

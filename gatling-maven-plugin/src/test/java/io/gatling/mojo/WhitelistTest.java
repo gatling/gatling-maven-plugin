25837fba83302aae54f86fbf28caafdb94994e7a
Alexander Gehret <alexander.gehret@lanzone.eu>
2017-03-31 10:25:13 +0200

    Removed whitespaces to ensure readability and corrected assertions

--- gatling-maven-plugin/src/test/java/io/gatling/mojo/WhitelistTest.java
+++ gatling-maven-plugin/src/test/java/io/gatling/mojo/WhitelistTest.java
@@ -19,7 +19,6 @@ public class WhitelistTest {
   @Test
   public void testWithoutAnyIncludesOrExcludes() throws Exception {
     GatlingMojo gatlingMojo = new GatlingMojo();
-
     String[] classFileList = new String[]{"io.test.great.class"};
 
     List<String> result = gatlingMojo.resolveIncludesAndExcludes(classFileList);
@@ -29,26 +28,22 @@ public class WhitelistTest {
   @Test
   public void testWithoutIncludesNotMatchingClassNames() throws Exception {
     String[] classFileList = new String[]{"io.test.great.class"};
-
     String[] includesList = new String[] {"io.notcool.*"};
     GatlingMojo mojo = new GatlingMojo();
 
     mojo.setIncludes(includesList);
 
-
     List<String> result = mojo.resolveIncludesAndExcludes(classFileList);
-    Assert.assertTrue(result.size() == 0);
+    Assert.assertTrue(result.isEmpty());
   }
 
   @Test
   public void testWithoutIncludesMatchingClassNames() throws Exception {
     String[] classFileList = new String[]{"io.test.great.class"};
-
     String[] includesList = new String[] {"io.test.*"};
     GatlingMojo mojo = new GatlingMojo();
 
     mojo.setIncludes(includesList);
-
 
     List<String> result = mojo.resolveIncludesAndExcludes(classFileList);
     Assert.assertTrue(result.size() == 1);
@@ -57,29 +52,24 @@ public class WhitelistTest {
   @Test
   public void testWithoutExcludesMatchingClassNames() throws Exception {
     String[] classFileList = new String[]{"io.test.great.class"};
-
     String[] excludesList = new String[] {"io.test.*"};
     GatlingMojo mojo = new GatlingMojo();
 
     mojo.setExcludes(excludesList);
 
-
     List<String> result = mojo.resolveIncludesAndExcludes(classFileList);
-    Assert.assertTrue(result.size() == 0);
+    Assert.assertTrue(result.isEmpty());
   }
 
   @Test
   public void testWithoutExcludesAndIncludesMatchingClassNames() throws Exception {
     String[] classFileList = new String[]{"io.test.great.class","io.anothertest.awesome.class","io.onemoretest.cool.class"};
-
     String[] excludesList = new String[] {"io.test.*"};
     String[] includesList = new String[] {"io.onemoretest.*"};
-
     GatlingMojo mojo = new GatlingMojo();
 
     mojo.setExcludes(excludesList);
     mojo.setIncludes(includesList);
-
 
     List<String> result = mojo.resolveIncludesAndExcludes(classFileList);
     Assert.assertTrue(result.size() == 1);
@@ -88,13 +78,10 @@ public class WhitelistTest {
   @Test
   public void testWithoutExcludesExactMatchingClassNames() throws Exception {
     String[] classFileList = new String[]{"io.test.great.class","io.anothertest.awesome.class","io.onemoretest.cool.class"};
-
     String[] excludesList = new String[] {"io.test.great"};
-
     GatlingMojo mojo = new GatlingMojo();
 
     mojo.setExcludes(excludesList);
-
 
     List<String> result = mojo.resolveIncludesAndExcludes(classFileList);
     Assert.assertTrue(result.size() == 2);
@@ -103,16 +90,12 @@ public class WhitelistTest {
   @Test
   public void testWithoutIncludesExactMatchingClassNames() throws Exception {
     String[] classFileList = new String[]{"io.test.great.class","io.test.great.work.class", "io.anothertest.awesome.class","io.onemoretest.cool.class"};
-
     String[] includesList = new String[] {"io.test.great"};
-
     GatlingMojo mojo = new GatlingMojo();
 
     mojo.setIncludes(includesList);
 
-
     List<String> result = mojo.resolveIncludesAndExcludes(classFileList);
     Assert.assertTrue(result.size() == 1);
   }
-
 }

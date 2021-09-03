4294e609cd468e151731ae79493a67734c2f415f
tpetillot <th.petillot@gmail.com>
2021-09-03 11:45:01 -0400

    chore: align plugins command name
    Motivation:
    Ease users comprehension
    
    Modifications:
    * Update deploy reference to upload
    * Unbind default deploy phase on EnterpriseUploadMojo
    
    Result:
    Command name is now enterpriseUpload
    
    Ref: MISC-243

--- src/main/java/io/gatling/mojo/EnterpriseUploadMojo.java
+++ src/main/java/io/gatling/mojo/EnterpriseUploadMojo.java
@@ -26,14 +26,13 @@ import org.apache.maven.plugin.AbstractMojo;
 import org.apache.maven.plugin.MojoExecutionException;
 import org.apache.maven.plugin.MojoFailureException;
 import org.apache.maven.plugins.annotations.Execute;
-import org.apache.maven.plugins.annotations.LifecyclePhase;
 import org.apache.maven.plugins.annotations.Mojo;
 import org.apache.maven.plugins.annotations.Parameter;
 import org.apache.maven.project.MavenProject;
 
 @Execute(goal = "enterprisePackage")
-@Mojo(name = "enterpriseDeploy", defaultPhase = LifecyclePhase.DEPLOY)
-public class EnterpriseDeployMojo extends AbstractMojo {
+@Mojo(name = "enterpriseUpload")
+public class EnterpriseUploadMojo extends AbstractMojo {
 
   @Parameter(defaultValue = "${project}", readonly = true)
   private MavenProject project;
